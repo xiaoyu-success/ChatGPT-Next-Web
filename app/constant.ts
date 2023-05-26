461d7428cd3987864d6ec06246e91993e96d6f11
xiaoyu01 <xiaoyu01@njfu.edu.cn>
2023-05-26 16:30:22 +0800

    fixed a bug regarding automatic updates

--- app/constant.ts
+++ app/constant.ts
@@ -7,6 +7,9 @@ export const OWNER = "xiaoyu-success"; // GitHub用户名
 export const REPO = "ChatGPT-Next-Web"; // GitHub仓库名称
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
+export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 // 导出图片/文件信息
